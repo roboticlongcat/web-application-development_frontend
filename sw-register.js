a3e69da4582b30945ce9248dc2579b7a68569e35
roboticlongcat <21orangesdisco@gmail.com>
2025-11-17 14:35:23 +0300

    Updates

---
+++ sw-register.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(function(registration) {
+        console.log('SW registered: ', registration);
+      })
+      .catch(function(registrationError) {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
