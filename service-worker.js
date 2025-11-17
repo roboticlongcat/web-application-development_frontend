18bcc50c1d9af91d3d59465f3155eba5e0632e51
roboticlongcat <21orangesdisco@gmail.com>
2025-11-17 20:10:30 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,77 +1,78 @@
-const CACHE_NAME = 'insulin-app-v1';
-const urlsToCache = [
-  '/',
-  './',
-  './index.html',
-  './static/js/bundle.js',
-  './static/css/main.css',
-  './manifest.json',
-  './default-patient.png',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
+// const CACHE_NAME = 'insulin-app-v1';
+// const urlsToCache = [
+//   '/',
+//   './',
+//   './index.html',
+//   './static/js/bundle.js',
+//   './static/css/main.css',
+//   './manifest.json',
+//   './default-patient.png',
+//   './icons/icon-192.png',
+//   './icons/icon-512.png'
+// ];
 
-self.addEventListener('install', (event) => {
-  console.log('Service Worker: Installing...');
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Service Worker: Caching files');
-        return cache.addAll(urlsToCache);
-      })
-      .then(() => self.skipWaiting())
-  );
-});
+// self.addEventListener('install', (event) => {
+//   console.log('Service Worker: Installing...');
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then((cache) => {
+//         console.log('Service Worker: Caching files');
+//         return cache.addAll(urlsToCache);
+//       })
+//       .then(() => self.skipWaiting())
+//   );
+// });
 
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker: Activated');
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            console.log('Service Worker: Clearing old cache');
-            return caches.delete(cache);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
-});
+// self.addEventListener('activate', (event) => {
+//   console.log('Service Worker: Activated');
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) => {
+//       return Promise.all(
+//         cacheNames.map((cache) => {
+//           if (cache !== CACHE_NAME) {
+//             console.log('Service Worker: Clearing old cache');
+//             return caches.delete(cache);
+//           }
+//         })
+//       );
+//     }).then(() => self.clients.claim())
+//   );
+// });
 
-self.addEventListener('fetch', (event) => {
-  // Не обрабатываем не-HTTP запросы
-  if (!event.request.url.startsWith('http')) {
-    return;
-  }
+// self.addEventListener('fetch', (event) => {
+//   // Не обрабатываем не-HTTP запросы
+//   if (!event.request.url.startsWith('http')) {
+//     return;
+//   }
 
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Возвращаем кэш если есть, иначе делаем запрос
-        if (response) {
-          return response;
-        }
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then((response) => {
+//         // Возвращаем кэш если есть, иначе делаем запрос
+//         if (response) {
+//           return response;
+//         }
         
-        return fetch(event.request).then((fetchResponse) => {
-          // Кэшируем только успешные ответы
-          if (!fetchResponse || fetchResponse.status !== 200 || fetchResponse.type !== 'basic') {
-            return fetchResponse;
-          }
+//         return fetch(event.request).then((fetchResponse) => {
+//           // Кэшируем только успешные ответы
+//           if (!fetchResponse || fetchResponse.status !== 200 || fetchResponse.type !== 'basic') {
+//             return fetchResponse;
+//           }
 
-          const responseToCache = fetchResponse.clone();
-          caches.open(CACHE_NAME)
-            .then((cache) => {
-              cache.put(event.request, responseToCache);
-            });
+//           const responseToCache = fetchResponse.clone();
+//           caches.open(CACHE_NAME)
+//             .then((cache) => {
+//               cache.put(event.request, responseToCache);
+//             });
 
-          return fetchResponse;
-        }).catch(() => {
-          // Fallback для картинок
-          if (event.request.destination === 'image') {
-            return caches.match('./default-patient.png');
-          }
-        });
-      })
-  );
-});
+//           return fetchResponse;
+//         }).catch(() => {
+//           // Fallback для картинок
+//           if (event.request.destination === 'image') {
+//             return caches.match('./default-patient.png');
+//           }
+//         });
+//       })
+//   );
+// });
+self.addEventListener('fetch',() => console.log("fetch"));
